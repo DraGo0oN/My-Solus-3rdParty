1e18794ff1ced1b6eddc98ad87c0ffbc7fbc6357
mrnitr0 <51184732+mrnitr0@users.noreply.github.com>
2021-07-15 09:48:40 +0200

    Update actions.py

--- tools/asbru-cm/actions.py
+++ tools/asbru-cm/actions.py
@@ -12,7 +12,7 @@ Suffix = "-1"
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf asbru-cm_6.2.2-1.deb" % (get.srcVERSION()))
+    shelltools.system("ar xf asbru-cm%s-1.deb" % (get.srcVERSION()))
     shelltools.system("tar xvf data.tar.xz")
     
 def install():
