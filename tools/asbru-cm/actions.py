26cb1718e870f75bc8cd5667155e5c6510204047
mrnitr0 <51184732+mrnitr0@users.noreply.github.com>
2021-07-15 09:50:20 +0200

    Update actions.py

--- tools/asbru-cm/actions.py
+++ tools/asbru-cm/actions.py
@@ -12,9 +12,8 @@ Suffix = "-1"
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf asbru-cm%s-1.deb" % (get.srcVERSION()))
+    shelltools.system("ar xf asbru-cm_6.2.2-1.deb")
     shelltools.system("tar xvf data.tar.xz")
-    
 def install():
     pisitools.insinto("/", "opt")
     pisitools.insinto("/", "usr")
